f8dac84ed74eea725f84b4a9fc6f26f4864a0cad
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2023-09-17 21:13:27 -0300

    Alterações visuais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatTreeModule } from '@angular/material/tree';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
+import { BrowserModule } from '@angular/platform-browser';
 
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
-import { SideMenuComponent } from './content/side-menu/side-menu.component';
 import { MainComponent } from './content/main/main.component';
+import { SideMenuComponent } from './content/side-menu/side-menu.component';
+import { HeaderComponent } from './header/header.component';
 
 import { AboutComponent } from './content/main/about/about.component';
 import { ExercisesComponent } from './content/main/exercises/exercises.component';
-import { MainPageComponent } from './content/main/main-page/main-page.component';
 import { VerbToBeComponent } from './content/main/exercises/verb-to-be/verb-to-be.component';
+import { MainPageComponent } from './content/main/main-page/main-page.component';
 
 
 
@@ -50,7 +52,8 @@ import { VerbToBeComponent } from './content/main/exercises/verb-to-be/verb-to-b
     MatTreeModule,
     MatCardModule,
     MatGridListModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
