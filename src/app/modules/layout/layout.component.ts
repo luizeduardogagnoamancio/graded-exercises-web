8398fa9fd24e6809d879b84329ffb44feac5f30a
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2025-03-02 15:27:47 -0300

    GRADEX-0001: Painel central e lateral direito estrutura

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { PainelLateralComponent } from './painel-lateral/painel-lateral.component';
 import { PainelSuperiorComponent } from './painel-superior/painel-superior.component';
 import { PainelCentralComponent } from './painel-central/painel-central.component';
-
+import { PainelLateralDireitoComponent } from './painel-lateral-direito/painel-lateral-direito.component';
 
 @Component({
   selector: 'app-layout',
   imports: [PainelLateralComponent,
             PainelSuperiorComponent,
-            PainelCentralComponent],
+            PainelCentralComponent,
+            PainelLateralDireitoComponent
+          ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
