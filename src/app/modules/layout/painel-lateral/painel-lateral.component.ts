62147db1f4045d46844e52f850adcb1429459a98
Luiz Eduardo Gagno Amancio <luiz.amancio@spassu.com.br>
2025-05-05 22:50:31 -0300

    GRADEX_0002: Botoes e fluxo finalizados

--- src/app/modules/layout/painel-lateral/painel-lateral.component.ts
+++ src/app/modules/layout/painel-lateral/painel-lateral.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'painel-lateral',
   templateUrl: './painel-lateral.component.html',
-  styleUrls: ['./painel-lateral.component.scss']
+  styleUrls: ['./painel-lateral.component.scss'],
+  imports: [ButtonModule, SelectButtonModule, RouterModule]
 })
 export class PainelLateralComponent implements OnInit {
 
