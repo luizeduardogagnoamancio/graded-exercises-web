af493ed13f7e9be41d38b0939d5feef76044cf67
Luiz Eduardo Gagno Amancio <luiz.amancio@spassu.com.br>
2025-05-27 20:14:40 -0300

    GRADEX_0003: Ajustes layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExercisesComponent } from './pages/exercises/exercises.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'exercises', component: ExercisesComponent },
+  { path: 'register', component: CadastroComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
