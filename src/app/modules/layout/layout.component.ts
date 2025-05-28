f658ff7dbca267065d38d54b551cdb5b526750ad
Luiz Eduardo Gagno Amancio <luiz.amancio@spassu.com.br>
2025-05-27 22:42:21 -0300

    GRADEX_0003: Responsividade, menu hamburguer

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -2,16 +2,34 @@ import { Component } from '@angular/core';
 import { PainelLateralComponent } from './painel-lateral/painel-lateral.component';
 import { PainelSuperiorComponent } from './painel-superior/painel-superior.component';
 import { PainelCentralComponent } from './painel-central/painel-central.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [PainelLateralComponent,
-            PainelSuperiorComponent,
-            PainelCentralComponent,
-          ],
+  imports: [
+    CommonModule, // Adicione aqui
+    PainelLateralComponent,
+    PainelSuperiorComponent,
+    PainelCentralComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  isMenuLateralAberto = false;
+  isTelaPequena = window.innerWidth <= 768;
 
+  constructor() {
+    // Atualiza isTelaPequena em redimensionamento
+    window.addEventListener('resize', () => {
+      this.isTelaPequena = window.innerWidth <= 768;
+      if (!this.isTelaPequena) {
+        this.isMenuLateralAberto = false;
+      }
+    });
+  }
+
+  toggleMenu(): void {
+    this.isMenuLateralAberto = !this.isMenuLateralAberto;
+  }
 }
--- src/app/modules/layout/painel-superior/painel-superior.component.ts
+++ src/app/modules/layout/painel-superior/painel-superior.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'painel-superior',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel-superior.component.scss']
 })
 export class PainelSuperiorComponent implements OnInit {
+  @Output() toggleMenuLateral = new EventEmitter<void>();
 
   constructor() { }
 
