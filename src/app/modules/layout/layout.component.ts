24353177c653c3784fd9ee89a9bf5782ac0c9ea8
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2025-08-11 20:09:13 -0300

    GRADEX_0003: Registro e login finalizados

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ImageFallbackDirective } from '../../directives/image-fallback.directive';
 
 
 @Component({
@@ -9,16 +10,31 @@ import { AuthService } from '../../services/auth.service';
   imports: [
     CommonModule,
     RouterOutlet,
-    RouterModule
+    RouterModule,
+    ImageFallbackDirective
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  @ViewChild('profileModal') profileModal!: ElementRef;
+  @ViewChild('profileTrigger') profileTrigger!: ElementRef;
   isSidebarVisible = false;
   isProfileModalVisible = false;
 
+  userProfileUrl = 'assets/images/default-avatar.png';
+
   constructor(public authService: AuthService) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const clickedInsideTrigger = this.profileTrigger.nativeElement.contains(event.target);
+    const clickedInsideModal = this.profileModal?.nativeElement.contains(event.target);
+
+    if (this.isProfileModalVisible && !clickedInsideTrigger && !clickedInsideModal) {
+      this.isProfileModalVisible = false;
+    }
+  }
 
   toggleSidebar(): void {
     this.isSidebarVisible = !this.isSidebarVisible;
